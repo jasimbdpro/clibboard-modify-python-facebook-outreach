f5c09eea292ee1e589075186f671a5e2478dca9d
Jasim Uddin <jasimbdpro@gmail.com>
2024-03-23 21:46:33 +0600

    new

--- clipboard_automator.py
+++ clipboard_automator.py
@@ -21,7 +21,10 @@ def main():
         if current_copied_text != last_copied_text:
             last_copied_text = current_copied_text
             if last_copied_text and not last_copied_text.startswith("Hello"):
-                text_to_paste = "Hello " + last_copied_text + ",\nI am Jasim Uddin, an expert Graphic Designer using Adobe Photoshop, Adobe Illustrator, Canva, etc., as primary tools, and I have satisfied over hundreds of clients over the last 5 years with my design abilities. If you need Social Media Posts, Stories, Reels for Instagram, Facebook, TikTok, etc., Web Banner Design, Email Template, and Signature Design, as well as Social Media Marketing services, feel free to contact me. I will be happy to answer any questions, ready to show my previous work examples, and client testimonials.\n\nBest Regards:\nJasim Uddin\njasimbdpro@gmail.com\n+8801827104738"
+                if last_copied_text.endswith('\n'):
+                    last_copied_text = last_copied_text[:-1]  # Remove the last character (newline)
+                text_to_paste = "Hello " + last_copied_text + ",\nI am Jasim Uddin, an expert Graphic Designer for Real Estate Agent using Adobe Photoshop, Adobe Illustrator, Canva etc as primary tools and I satisfied over hundreds of clients over last 5 years by creating amazing design for their business.  If you want me to create Social Media Posts Design, Story, Reels for Instagram, Facebook, Tiktok business etc, Web Banner Design, Email Template and Signature Design, Social Media Marketing services, feel free to contact me. I am happy to answer any questions, and I am ready to show my previous creation/work examples, and client testimonials.\n\nBest Regards:\nJasim Uddin\njasimbdpro@gmail.com\n+8801827104738"
+
                 copy_to_clipboard(text_to_paste)
                 print("Text modified and copied to clipboard:", text_to_paste)
         time.sleep(1)  # Adjust sleep time as needed
